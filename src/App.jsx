8203328dc9da8fb690692f00b2e6d93068049992
afiella <ellabellosei@gmail.com>
2025-05-10 14:01:34 -0400

    updating app.jsx to include roomselectpage route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/public/LandingPage';
+import RoomSelectPage from './pages/public/RoomSelectPage';
 import { AuthProvider } from './context/AuthContext';
 import { initializeRooms } from './firebase/initializeFirestore';
 
@@ -17,6 +18,7 @@ export default function App() {
         <Routes>
           {/* Public routes */}
           <Route path="/" element={<LandingPage />} />
+          <Route path="/select" element={<RoomSelectPage />} />
         </Routes>
       </HashRouter>
     </AuthProvider>
