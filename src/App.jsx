c915ccf92ba7730bd309fa16e0dd2bdecda2b13b
afiella <ellabellosei@gmail.com>
2025-05-10 14:20:28 -0400

    Add ModeratorLoginPage and AdminLoginPage with routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/public/LandingPage';
 import RoomSelectPage from './pages/public/RoomSelectPage';
+import ModeratorLoginPage from './pages/public/ModeratorLoginPage';
+import AdminLoginPage from './pages/public/AdminLoginPage';
 import { AuthProvider } from './context/AuthContext';
 import { initializeRooms } from './firebase/initializeFirestore';
 
@@ -19,6 +21,8 @@ export default function App() {
           {/* Public routes */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/select" element={<RoomSelectPage />} />
+          <Route path="/mod-login" element={<ModeratorLoginPage />} />
+          <Route path="/admin-login" element={<AdminLoginPage />} />
         </Routes>
       </HashRouter>
     </AuthProvider>
